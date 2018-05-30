43897196f8896e08fbffa96a6168d4911cd71b62
Kyle Thomson <kjay.thomson@gmail.com>
2018-05-30 17:04:00 +1000

    Generate pages using the CMS

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,118 +1,42 @@
-const path = require('path');
-exports.createPages = ({boundActionCreators, graphql}) => {
-  const {createPage} = boundActionCreators;
+const path = require("path");
 
-  const postTemplate = path.resolve('src/components/Header.js');
+exports.createPages = ({ boundActionCreators, graphql }) => {
+  const { createPage } = boundActionCreators;
 
-  return graphql(`{
-    allMarkdownRemark {
-      edges {
-        node{
-          html
-          id
-          frontmatter{
-            path
-            title
+  return new Promise((resolve, reject) => {
+    let Component = path.resolve("./src/pages/cms_page.js");
+    resolve(
+      graphql(`
+        {
+          allMarkdownRemark {
+            edges {
+              node {
+                frontmatter {
+                  title
+                  path
+                  sections {
+                    type
+                    heading
+                  }
+                }
+              }
+            }
           }
-        } 
-      }
-    }
-  }`).then(res => {
-    if(res.errors) {
-      return Promise.reject(res.errors);
-    }
-      res.data.allMarkdownRemark.edges.forEach(({node}) => {
-        createPage({
-          path: node.frontmatter.path,
-          component: postTemplate
-        })
-      });
-  })
-}
-// const _ = require('lodash')
-// const path = require('path')
-// const { createFilePath } = require('gatsby-source-filesystem')
-
-// exports.createPages = ({ boundActionCreators, graphql }) => {
-//   const { createPage } = boundActionCreators
-
-//   return graphql(`
-//     {
-//       allMarkdownRemark(limit: 1000) {
-//         edges {
-//           node {
-//             id
-//             fields {
-//               slug
-//             }
-//             frontmatter {
-//              title
-//              tags
-//               templateKey
-//             }
-//           }
-//         }
-//       }
-//     }
-//   `).then(result => {
-//     if (result.errors) {
-//       result.errors.forEach(e => console.error(e.toString()))
-//       return Promise.reject(result.errors)
-//     }
-
-//     const posts = result.data.allMarkdownRemark.edges
-
-//     posts.forEach(edge => {
-//       const id = edge.node.id
-//       createPage({
-//         path: edge.node.fields.slug,
-//         tags: edge.node.frontmatter.tags,
-//         component: path.resolve(
-//           `src/components/${String(edge.node.frontmatter.templateKey)}.js`
-//         ),
-//         // additional data can be passed via context
-//         context: {
-//           id,
-//         },
-//       })
-//     })
-
-//     // Tag pages:
-//     let tags = []
-//     // Iterate through each post, putting all found tags into `tags`
-//     posts.forEach(edge => {
-//       if (_.get(edge, `node.frontmatter.tags`)) {
-//         tags = tags.concat(edge.node.frontmatter.tags)
-//       }
-//     })
-//     // Eliminate duplicate tags
-//     tags = _.uniq(tags)
-
-//     // Make tag pages
-//     tags.forEach(tag => {
-//       const tagPath = `/tags/${_.kebabCase(tag)}/`
-
-//       createPage({
-//         path: tagPath,
-//         component: path.resolve(`src/components/header.js`),
-//         context: {
-//           tag,
-//         },
-//       })
-//     })
-//   })
-// }
-
-// exports.onCreateNode = ({ node, boundActionCreators, getNode }) => {
-//   const { createNodeField } = boundActionCreators
-
-//   if (node.internal.type === `MarkdownRemark`) {
-//     const value = createFilePath({ node, getNode })
-//     createNodeField({
-//       name: `slug`,
-//       node,
-//       value,
-//     })
-//   }
-// }
-
+        }
+      `).then(res => {
+        if (res.errors || res.messages) {
+          reject(res.errors + " " + res.messages);
+        }
+        res.data.allMarkdownRemark.edges.forEach(({ node }) => {
+          createPage({
+            path: node.frontmatter.path,
+            component: Component,
+            context: {
+              sections: node.frontmatter.sections
+            }
+          });
+        });
+      })
+    );
+  });
+};
