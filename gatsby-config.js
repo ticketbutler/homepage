470a00aa6b44cda52e262e878ca4da137481ac6e
Kyle Thomson <kjay.thomson@gmail.com>
2018-05-30 17:03:33 +1000

    Move pages to ./pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,44 +1,34 @@
 module.exports = {
-  pathPrefix: `/TicketButler`,
-  siteMetadata: {
-    title: 'Gatsby Default Starter',
-    
-  },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-catch-links',
-    'styled-jsx/babel',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-catch-links",
+    "styled-jsx/babel",
     `gatsby-plugin-styled-components`,
     // `gatsby-plugin-google-fonts`,
-    'gatsby-plugin-netlify-cms',
+    "gatsby-plugin-netlify-cms",
     `gatsby-plugin-sharp`,
-    'gatsby-transformer-sharp',
+    "gatsby-transformer-sharp",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/pages`,
-        name: `pages`,
-        
-  
-        
-      },
+        path: __dirname + "/pages",
+        name: `pages`
+      }
     },
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
-          'gatsby-remark-copy-linked-files',
+          "gatsby-remark-copy-linked-files",
           {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 800,
               linkImagesToOriginal: false
-            },
+            }
           }
         ]
       }
-    },
-    ],
-    
-    
-}
+    }
+  ]
+};
