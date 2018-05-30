17703718d70cd0e5708bb0ff8da285cb0b7d634e
Kyle Thomson <kjay.thomson@gmail.com>
2018-05-30 18:33:29 +1000

    Clean up styling

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,10 @@
 import * as React from "react";
 
-const Hero = ({ heading, description }) => (
+const Hero = ({ heading, text }) => (
   <div
     style={{
       display: "inline-block",
       width: "100%",
-      marginTop: 100,
       textAlign: "center",
       padding: "60px 110px",
       zIndex: -1
@@ -13,14 +12,10 @@ const Hero = ({ heading, description }) => (
   >
     <h1
       style={{
-        width: 1025,
-        marginBottom: 37,
         display: "block",
-        color: "#FFFFFF",
         fontFamily: "Hind",
         fontSize: 75,
         fontWeight: "bold",
-        lineHeight: 70,
         textAlign: "center",
         zIndex: -1
       }}
@@ -32,14 +27,13 @@ const Hero = ({ heading, description }) => (
         width: 604,
         marginBottom: 37,
         display: "block",
-        color: "#FFFFFF",
         fontFamily: "Montserrat",
         fontSize: 25,
         lineHeight: 30,
         textAlign: "center"
       }}
     >
-      {description}
+      {text}
     </p>
     <a href="#">
       <span>Get started, it's free!</span>
