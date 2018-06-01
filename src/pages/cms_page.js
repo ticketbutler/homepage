720749251304ac9110ca70d2e3c552b9001a1299
Kyle Thomson <kjay.thomson@gmail.com>
2018-06-01 19:08:02 +1000

    Remove double layout component

--- src/pages/cms_page.js
+++ src/pages/cms_page.js
@@ -3,7 +3,7 @@ import Layout from "../layouts";
 
 // import Hero from "../components/Hero";
 import FutureImageRight from "../components/future_image_right";
-import FutureImageIeft from  "../components/future_image_left";
+import FutureImageIeft from "../components/future_image_left";
 import TallCenteredFuture from "../components/tall_centered_future";
 import FutureImageRightSecond from "../components/future_image_right_second";
 import FutureLayers from "../components/future_layers";
@@ -16,22 +16,18 @@ const components = [
   { Component: TallCenteredFuture, id: "tall_centered_future" },
   { Component: FutureImageRightSecond, id: "future_image_right_second" },
   { Component: FutureLayers, id: "future_layers" },
-  { Component: Contact, id: "contact" },
-  
-  
+  { Component: Contact, id: "contact" }
 ];
 
 const CmsPage = ({ pathContext }) => (
-  <Layout>
-    <div>
-      {pathContext.sections &&
-        pathContext.sections.map(section => {
-          const Component = components.find(({ id }) => id === section.type)
-            .Component;
-          return <Component {...section} />;
-        })}
-    </div>
-  </Layout>
+  <div>
+    {pathContext.sections &&
+      pathContext.sections.map(section => {
+        const Component = components.find(({ id }) => id === section.type)
+          .Component;
+        return <Component {...section} />;
+      })}
+  </div>
 );
 
 export default CmsPage;
