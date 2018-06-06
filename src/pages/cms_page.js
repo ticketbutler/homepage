968e76d87ce0732c6c6d064ec4d7c8e0c49b12d6
Daryan Kamalifar <32463453+Daryan98@users.noreply.github.com>
2018-06-06 14:41:04 +0200

    fixing the navbar section for screen below 1080

--- src/pages/cms_page.js
+++ src/pages/cms_page.js
@@ -3,14 +3,14 @@ import * as React from "react";
 
 
 import Navbar from "../components/navbar";
-import Header from "../components/header";
+import Header from "../components/Header";
 import FutureImageRight from "../components/future_image_right";
 import FutureImageIeft from "../components/future_image_left";
 import TallCenteredFuture from "../components/tall_centered_future";
 import FutureImageRightSecond from "../components/future_image_right_second";
 import FutureLayers from "../components/future_layers";
-import Contact from "../components/contact";
-import Footer from "../components/footer";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 
 
 const components = [
