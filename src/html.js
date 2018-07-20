25dbf4174f415d43386b6182ace85b39cb595cb5
Daryan Kamalifar <32463453+Daryan98@users.noreply.github.com>
2018-07-20 12:01:03 +0200

    fixing some easlint warning from html.js

--- src/html.js
+++ src/html.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 let stylesStr;
-if (process.env.NODE_ENV === `production`) {
+if (process.env.NODE_ENV === "production") {
   try {
-    stylesStr = require(`!raw-loader!../public/styles.css`);
+    stylesStr = require("!raw-loader!../public/styles.css");
   } catch (e) {
     console.log(e);
   }
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === `production`) {
 module.exports = class HTML extends React.Component {
   render() {
     let css;
-    if (process.env.NODE_ENV === `production`) {
+    if (process.env.NODE_ENV === "production") {
       css = (
         <style
           id="gatsby-inlined-css"
