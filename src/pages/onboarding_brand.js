bb962497480058d2753b7f61f002b8f5b092d7c1
vercamach <machackovaver@gmail.com>
2019-01-14 16:23:02 +0100

    finishing styling ticket box and making it react to font change

--- src/pages/onboarding_brand.js
+++ src/pages/onboarding_brand.js
@@ -3,6 +3,7 @@ import { SketchPicker } from "react-color";
 import { ClickedOutside } from "../components/elements/helpers";
 import Dropzone from "react-dropzone";
 import UploadLogo from "../img/upload-logo.png";
+import { Button } from "../components/elements/elements";
 const styles = {
   h2: {
     fontSize: "18px",
@@ -22,13 +23,23 @@ const styles = {
     border: "1px solid #C5D0DE",
     marginTop: "8px",
     height: "46px"
+  },
+  div: {
+    marginTop: "20px"
+  },
+  h4: {
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    marginBottom: "10px",
+    color: "rgb(51, 51, 51)",
+    fontSize: "14px"
   }
 };
 
 class Form extends React.Component {
   state = {
     colorPickerOpen: false,
-    brand_colour: "blue",
+    brand_colour: "#FDF4A4",
     logo: "",
     font: "",
     bank_details: {
@@ -51,9 +62,13 @@ class Form extends React.Component {
     return (
       <img
         src={this.state.logo ? this.state.logo.preview : this.props.logo[0]}
-        style={{ width: "100%", height: "100%", objectFit: "cover" }}
+        css={{ width: "100%", height: "100%", objectFit: "cover" }}
       />
     );
+  };
+
+  changeFont = event => {
+    this.setState({ font: event.target.value });
   };
   render() {
     return (
@@ -61,7 +76,7 @@ class Form extends React.Component {
         <header
           css={{
             height: " 80px",
-            width: "px",
+
             backgroundColor: "#FFFFFF",
             boxShadow: "3px 3px 4px 0 #F0F2F5"
           }}
@@ -71,25 +86,28 @@ class Form extends React.Component {
             css={{ maxWidth: "142px", marginLeft: "38px", marginTop: "30px" }}
           />
         </header>
-        <div css={{ marginLeft: "20%", marginRight: "20%", marginTop: "80px" }}>
+        <div
+          css={{
+            marginLeft: "12%",
+            marginRight: "12%",
+            marginTop: "80px",
+            marginBottom: "80px"
+          }}
+        >
           <h3 css={{ fontWeight: "600px", fontSize: "18px" }}> STEP 1 OF 3</h3>
           <h1 css={{ fontWeight: "600px", fontSize: "36px" }}>
-            Use your brand
+            Desing your brand
           </h1>
 
-          <h2 css={styles.h2}>
-            It's all about your business!
-            <span
-              css={{ display: "block", paddingTop: "8px", maxWidth: "560px" }}
-            >
-              It’s all about your business! Your brand is displayed across the
-              whole ticketing experience on the event page, in emails and on the
-              tickets.
-            </span>
+          <h2 css={{ ...styles.h2, maxWidth: "560px" }}>
+            It’s all about your business! Your brand is displayed across the
+            whole ticketing experience on the event page, in emails and on the
+            tickets.
           </h2>
+
           <div style={{ display: "flex", justifyContent: "space-between" }}>
             <div>
-              <div css={{ marginTop: "40px" }}>
+              <div css={styles.div}>
                 <label css={styles.label}>
                   select a font that goes with your brand
                 </label>
@@ -101,16 +119,17 @@ class Form extends React.Component {
                     color: "#C5D0DE",
                     fontSize: "14px"
                   }}
+                  onChange={this.changeFont}
                 >
-                  <option value="courierNew" style={{ color: "pink" }}>
-                    Courier New
+                  <option value="Courier New">Courier New</option>
+                  <option value="Georgia">Georgia</option>
+                  <option value="Lucida Console">Lucida Console</option>
+                  <option value="Lucida Sans Unicode">
+                    Lucida Sans Unicode
                   </option>
-                  <option value="georgia">Georgia</option>
-                  <option value="lucidaConsole">Lucida Console</option>
-                  <option value="lucidaSansUnicode">Lucida Sans Unicode</option>
-                  <option value="tahoma">Tahoma</option>
-                  <option value="timesNewRoman">Times New Roman</option>
-                  <option value="verdana">Verdana</option>
+                  <option value="Tahoma">Tahoma</option>
+                  <option value="Times New Roman">Times New Roman</option>
+                  <option value="Verdana">Verdana</option>
                 </select>
                 <div css={{ marginTop: "30px" }}>
                   <label css={styles.label}>select a color</label>
@@ -162,32 +181,31 @@ class Form extends React.Component {
                     {({ getRootProps, getInputProps }) => {
                       return (
                         <div
-                          style={{
-                            height: 64,
-                            maxWidth: 348,
+                          css={{
+                            height: "64px",
+                            maxWidth: "348px",
                             border: "2px dashed #C5D0DE",
-                            borderRadius: 6,
+                            borderRadius: "6px",
                             backgroundColor: "#FFFFFF",
                             boxShadow: "0 2px 9px 0 rgba(0,0,0,0.03)",
-                            marginTop: 7
+                            marginTop: "7px"
                           }}
                           {...getRootProps()}
                         >
-                          <div style={{ textAlign: "center", marginTop: 20 }}>
+                          <div css={{ textAlign: "center", marginTop: "20px" }}>
                             <img
-                              style={{ width: 20, marginBottom: "-5px" }}
+                              css={{ width: "20px", marginBottom: "-5px" }}
                               src={UploadLogo}
                             />
                             <p
-                              style={{
+                              css={{
                                 padding: 0,
-                                marginLeft: 13,
+                                marginLeft: "13px",
                                 marginBottom: 0,
-
                                 color: "#C5D0DE",
                                 textTransform: "uppercase",
-                                fontSize: 11,
-                                letterSpacing: 1,
+                                fontSize: "11px",
+                                letterSpacing: "1px",
                                 display: "inline"
                               }}
                             >
@@ -203,11 +221,118 @@ class Form extends React.Component {
                 </div>
               </div>
             </div>
-            <div style={{ marginTop: 40 }}>
-              <h2 style={styles.label}>
+            <div css={styles.div}>
+              <h2 css={{ ...styles.label, marginBottom: "12px" }}>
                 Preview of a simple homepage integration
               </h2>
+              <div
+                css={{
+                  height: "286px",
+                  width: "526px",
+                  boxShadow: "rgb(212, 217, 223) 2px 2px 8px 0px",
+                  fontFamily: "Times New Roman"
+                }}
+              >
+                <div
+                  css={{
+                    marginLeft: "30px",
+                    marginRight: "30px"
+                  }}
+                >
+                  <div
+                    style={{
+                      paddingTop: "50px",
+                      display: "flex",
+                      flexWrap: "wrap",
+                      justifyContent: "space-between",
+
+                      borderBottom: "1px solid #EEEEEE"
+                    }}
+                  >
+                    <div>
+                      <h4 css={{ ...styles.h4, fontFamily: this.state.font }}>
+                        DEMO TICKET NAME
+                      </h4>
+                      <p
+                        style={{
+                          fontSize: "12px",
+                          fontWeight: 500,
+                          color: "#636E6D",
+                          fontFamily: this.state.font
+                        }}
+                      >
+                        Demo text to show how your font looks
+                      </p>
+                    </div>
+                    <div css={{ marginRight: "-43px", marginTop: "8px" }}>
+                      <span css={{ ...styles.h4, fontFamily: this.state.font }}>
+                        99.00 kr{" "}
+                      </span>{" "}
+                      <span
+                        css={{ fontSize: "10px", fontFamily: this.state.font }}
+                      >
+                        + 7.61kr fee
+                      </span>
+                    </div>
+                    <div css={{ marginTop: "8px" }}>
+                      <select css={{ background: "white" }}>
+                        <option> 3</option>{" "}
+                      </select>
+                    </div>
+                  </div>
+                  <p
+                    css={{
+                      margin: "7px 0px",
+                      fontSize: "11px",
+                      fontFamily: this.state.font
+                    }}
+                  >
+                    Price include 25% VAT
+                  </p>
+
+                  <div
+                    css={{
+                      height: "53px",
+                      width: "120px",
+                      border: "none",
+                      background: this.state.brand_colour,
+                      fontSize: "13px",
+                      fontWeight: "bold",
+                      display: "flex",
+                      placeContent: "center",
+                      color: "black",
+                      padding: " 20px 10px",
+                      marginTop: "35px",
+                      float: "right",
+                      fontFamily: this.state.font
+                    }}
+                  >
+                    Buy tickets
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
+          <div
+            css={{
+              marginTop: "150px",
+              display: "flex",
+              justifyContent: "space-between"
+            }}
+          >
+            <h2 css={{ ...styles.h2, paddingTop: "20px" }}>
+              {" "}
+              Need help? Call us at 89 80 12 80.{" "}
+            </h2>
+            <Button
+              style={{
+                background:
+                  "linear-gradient(112.11deg, #326DE9 0%, #7E52E8 100%)",
+                width: "25%"
+              }}
+            >
+              Next step{" "}
+            </Button>
           </div>
         </div>
       </div>
