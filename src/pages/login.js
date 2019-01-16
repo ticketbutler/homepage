21fd643516ffcc5b254d8796eba624504d3569d1
Edita <super@Supers-MacBook-Pro.local>
2019-01-16 10:15:48 +0100

    Adding Button and Form element to page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { OnboardingHeader } from "../components/elements/onboarding_header";
 import { styles } from "../layouts/style";
-import { Button } from "../components/elements/elements";
+import { Button, Form } from "../components/elements/onboarding_elements";
 import ButlerLogo from "../../static/img/Butler.png";
 import { CancelIcon } from "../components/elements/icons";
 
@@ -60,50 +60,66 @@ class LogIn extends React.Component {
             >
               Log in
             </h1>
-            <div
-              css={{
-                maxWidth: "444px",
-                width: "100%",
-                margin: "0 auto"
-              }}
-            >
-              <p
-                css={{
-                  textAlign: "left",
-                  color: "#89909B",
-                  margin: 0,
-                  fontSize: "12px",
-                  fontWeight: "500",
-                  letterSpacing: "1.2px",
-                  lineHeight: "21px"
-                }}
-              >
-                YOUR EMAIL
-              </p>
-            </div>
-            <input
-              css={{
-                ...styles.input,
-                margin: "0.5em auto",
-                padding: "10px"
-              }}
-            />
+            <Form validateFields={{ email: "exists" }}>
+              {({
+                values,
+                updateForm,
+                submitForm,
+                validationErrors,
+                requestError
+              }) => {
+                return (
+                  <form>
+                    <div
+                      css={{
+                        maxWidth: "444px",
+                        width: "100%",
+                        margin: "0 auto"
+                      }}
+                    >
+                      <p
+                        css={{
+                          textAlign: "left",
+                          color: "#89909B",
+                          margin: 0,
+                          fontSize: "12px",
+                          fontWeight: "500",
+                          letterSpacing: "1.2px",
+                          lineHeight: "21px"
+                        }}
+                      >
+                        YOUR EMAIL
+                      </p>
+                    </div>
+                    <input
+                      required
+                      type="email"
+                      css={{
+                        ...styles.input,
+                        margin: "0.5em auto",
+                        padding: "10px"
+                      }}
+                    />
 
-            <Button
-              style={{
-                background:
-                  "linear-gradient(112.11deg, #326DE9 0%, #7E52E8 100%)",
-
-                margin: "1.5em auto",
-                "@media(max-width: 420px)": {
-                  width: "0%",
-                  fontSize: "14px",
-                  padding: "15px 30px"
-                }
+                    <Button
+                      buttonStyle={{
+                        background:
+                          "linear-gradient(112.11deg, #326DE9 0%, #7E52E8 100%)",
+                        margin: "1.5em auto",
+                        width: "30%",
+                        fontSize: "14px",
+                        "@media(max-width: 500px)": {
+                          width: "50%",
+                          padding: "15px 30px"
+                        }
+                      }}
+                    >
+                      Log in
+                    </Button>
+                  </form>
+                );
               }}
-            >
-              Log in
-            </Button>
+            </Form>
             <a css={{ textDecoration: "none" }} href="/onboarding_brand">
               <p
                 css={{
