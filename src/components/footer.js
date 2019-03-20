d06376c3604812e376bf8428deb0b2387678c0f5
editagud <edit0796@stud.kea.dk>
2019-03-20 15:37:00 +0100

    adding button link for self onboarding

--- src/components/footer.js
+++ src/components/footer.js
@@ -62,7 +62,7 @@ const Footer = ({ linkLists }) => (
       <ul>
         <div style={{ display: "grid" }}>
           <a href="/">
-            <img src="/img/logo_blue.png" style={{ maxWidth: 200 }} />
+            <img src="/img/logo_purple.png" style={{ maxWidth: 200 }} />
           </a>
 
           <span
