9aa50173e9ecc85ed7d55165a5463823615083ea
editagud <edit0796@stud.kea.dk>
2019-03-27 13:47:04 +0100

    add ClickedOutside component from tb components

--- src/components/elements/modal.js
+++ src/components/elements/modal.js
@@ -1,74 +1,74 @@
 import React from "react";
 import { CancelIcon } from "../elements/icons";
-import { ClickedOutside } from "tb-components";
+import { ClickedOutside } from "@ticketbutler/components";
 
 export const Modal = props => {
   return (
-    <ClickedOutside
-      onClickOutside={() => {
-        props.isOpen(false);
+    <div
+      style={{
+        zIndex: 100,
+        top: 0,
+        display: "flex",
+        flexDirection: "column",
+        transition: "all 200ms",
+        ...(props.isOpen
+          ? {
+              opacity: 1,
+              visibility: "visible"
+            }
+          : {
+              opacity: 0,
+              visibility: "hidden"
+            })
       }}
     >
+      {" "}
       <div
         style={{
-          zIndex: 100,
+          position: "fixed",
           top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          backgroundColor: "#10234D",
+          opacity: 0.3,
+          zIndex: 100
+        }}
+      />
+      <div
+        style={{
           display: "flex",
           flexDirection: "column",
-          transition: "all 200ms",
-          ...(props.isOpen
-            ? {
-                opacity: 1,
-                visibility: "visible"
-              }
-            : {
-                opacity: 0,
-                visibility: "hidden"
-              })
+          position: "fixed",
+          top: "53%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          transition: "500ms opacity",
+          zIndex: 100,
+          width: "80%",
+          margin: "o auto",
+          height: "auto"
         }}
       >
-        {" "}
-        <div
+        <span
           style={{
-            position: "fixed",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            backgroundColor: "#10234D",
-            opacity: 0.3,
-            zIndex: 100
+            possition: "fixed",
+            textAlign: "right",
+            width: "105%",
+            color: "white",
+            fontSize: 25,
+            margin: "10px 10px 0px 0px"
           }}
-        />
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            position: "fixed",
-            top: "53%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            transition: "500ms opacity",
-            zIndex: 100,
-            width: "80%",
-            margin: "o auto",
-            height: "auto"
+          onClick={() => props.toogle(false)}
+        >
+          {" "}
+          <CancelIcon />
+        </span>
+        <ClickedOutside
+          onClickOutside={() => {
+            props.toogle(false);
           }}
         >
-          <span
-            style={{
-              possition: "fixed",
-              textAlign: "right",
-              width: "105%",
-              color: "white",
-              fontSize: 25,
-              margin: "10px 10px 0px 0px"
-            }}
-            onClick={() => props.toogle(false)}
-          >
-            {" "}
-            <CancelIcon />
-          </span>
           <div
             style={{
               position: "relative",
@@ -97,8 +97,8 @@ export const Modal = props => {
               />
             )}
           </div>
-        </div>
+        </ClickedOutside>
       </div>
-    </ClickedOutside>
+    </div>
   );
 };
