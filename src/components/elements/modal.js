fd68166c5765cf9e2db19f742f7774eaa86668fc
editagud <edit0796@stud.kea.dk>
2019-03-27 11:53:31 +0100

    creating modal component

---
+++ src/components/elements/modal.js
@@ -0,0 +1,104 @@
+import React from "react";
+import { CancelIcon } from "../elements/icons";
+import { ClickedOutside } from "tb-components";
+
+export const Modal = props => {
+  return (
+    <ClickedOutside
+      onClickOutside={() => {
+        props.isOpen(false);
+      }}
+    >
+      <div
+        style={{
+          zIndex: 100,
+          top: 0,
+          display: "flex",
+          flexDirection: "column",
+          transition: "all 200ms",
+          ...(props.isOpen
+            ? {
+                opacity: 1,
+                visibility: "visible"
+              }
+            : {
+                opacity: 0,
+                visibility: "hidden"
+              })
+        }}
+      >
+        {" "}
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "#10234D",
+            opacity: 0.3,
+            zIndex: 100
+          }}
+        />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            position: "fixed",
+            top: "53%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            transition: "500ms opacity",
+            zIndex: 100,
+            width: "80%",
+            margin: "o auto",
+            height: "auto"
+          }}
+        >
+          <span
+            style={{
+              possition: "fixed",
+              textAlign: "right",
+              width: "105%",
+              color: "white",
+              fontSize: 25,
+              margin: "10px 10px 0px 0px"
+            }}
+            onClick={() => props.toogle(false)}
+          >
+            {" "}
+            <CancelIcon />
+          </span>
+          <div
+            style={{
+              position: "relative",
+              paddingBottom: "56.25%" /* 16:9 */,
+
+              height: 0
+            }}
+          >
+            {props.isOpen && (
+              <iframe
+                style={{
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  width: "100%",
+                  height: "100%"
+                }}
+                id="ytplayer"
+                type="text/html"
+                src={props.video + "?autoplay=1"}
+                frameBorder="0"
+                webkitAllowFullScreen
+                mozallowfullscreen
+                allowFullScreen
+                allow="autoplay; encrypted-media"
+              />
+            )}
+          </div>
+        </div>
+      </div>
+    </ClickedOutside>
+  );
+};
