72d4c9fdb0b7fb7514cae219da2b7ace745d9cee
editagud <edit0796@stud.kea.dk>
2019-03-29 14:12:58 +0100

    adding linkedin insight tag

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -174,5 +174,29 @@ export const Layout = ({
       defer
       src="//js.hs-scripts.com/3835323.js"
     />
+
+    <script type="text/javascript">
+      dangerouslySetInnerHTML={{
+        __html: `
+ _linkedin_partner_id = "975602";
+ window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+ window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+ </script><script type="text/javascript">
+ (function(){var s = document.getElementsByTagName("script")[0];
+ var b = document.createElement("script");
+ b.type = "text/javascript";b.async = true;
+ b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+ s.parentNode.insertBefore(b, s);})();`
+      }}
+    </script>
+    <noscript>
+      <img
+        height="1"
+        width="1"
+        style="display:none;"
+        alt=""
+        src="https://dc.ads.linkedin.com/collect/?pid=975602&fmt=gif"
+      />
+    </noscript>
   </div>
 );
