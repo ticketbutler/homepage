fd9097d79d4af86955a903c65161589b319afb4c
editagud <edit0796@stud.kea.dk>
2019-04-03 17:30:32 +0200

    cleaning future layer component code

--- src/components/future_layers.js
+++ src/components/future_layers.js
@@ -1,7 +1,6 @@
 import React from "react";
-
-import { Button, SectionText } from "./elements/elements";
-
+import { mq } from "../styles";
+import { Button } from "./elements/elements";
 import { SmallTriangle } from "./elements/shapes";
 
 const FutureLayers = ({
@@ -12,42 +11,110 @@ const FutureLayers = ({
   image_alt,
   button_link
 }) => (
-  <section className="future_layer">
-    <img alt={image_alt} className="content_image" src={image} />
+  <section
+    css={{
+      width: "100%",
+      position: "relative",
+      padding: "110px 0",
+      paddingLeft: 0,
+      zIndex: 1,
+      marginTop: "330px",
+      background: "linear-gradient(311.59deg, #28d8a7 0%, #1dc9ca 100%)"
+    }}
+  >
+    <img
+      alt={image_alt}
+      src={image}
+      css={{
+        width: "60%",
+        position: "absolute",
+        left: "0%",
+        zIndex: "-1",
+        marginTop: "-300px",
+        marginLeft: "50px",
+        transition: "0.3s",
+        boxShadow: "5px 5px 10px -4px rgba(0, 0, 0, 0.15)",
+        [mq(1000)]: {
+          position: "absolute",
+          top: "-220px",
+          margin: 0,
+          float: "left",
+          left: "50%",
+          transform: "translateX(-50%)",
+          width: "70%"
+        },
+        [mq(700)]: {
+          width: "100%"
+        },
+        [mq(400)]: {
+          top: "-60px"
+        }
+      }}
+    />
 
-    <div className="section_content">
-      <SectionText>
+    <div
+      css={{
+        height: "auto",
+        width: "55%",
+        margin: "30px",
+        backgroundColor: "#FFFFFF",
+        boxShadow: "0px 0px 12px -4px rgba(0,0,0,0.35)",
+        position: "relative",
+        zIndex: 1001,
+        float: "right",
+        padding: "80px",
+        transition: "all .3s ease-out",
+        [mq(1000)]: {
+          width: "80%",
+          margin: "0px",
+          marginRight: "10%",
+          [mq(750)]: {
+            marginRight: "0px",
+            width: "100%",
+            padding: "30px"
+          }
+        }
+      }}
+    >
+      <div>
         <h2
-          style={{
-            textAlign: "left",
-            float: "left",
-            display: "block"
+          css={{
+            fontSize: "46px",
+            lineHeight: "54px",
+            margin: "15px 0",
+            color: "#333f52",
+            fontFamily: "Hind",
+            fontWeight: "bold",
+            [mq(1350)]: {
+              fontSize: "32px",
+              lineHeight: "38px",
+              textAlign: "center"
+            }
           }}
         >
           {heading}
         </h2>
         <p
-          style={{
-            textAlign: "left",
-            float: "left",
-            display: "block"
+          css={{
+            margin: "15px 0",
+            fontSize: "24px",
+            lineHeight: 1.5,
+            color: "#6c7582",
+            [mq(1350)]: {
+              fontSize: "18px",
+              lineHeight: 1.6
+            }
           }}
         >
           {text}
         </p>
         {button_text &&
           button_text.length > 0 && (
-            <Button
-              to={button_link}
-              className="right"
-              style={{
-                right: "100%"
-              }}
-            >
-              {button_text}
-            </Button>
+            <div css={{ display: "flex", justifyContent: "center" }}>
+              <Button to={button_link}>{button_text} </Button>
+            </div>
           )}
-      </SectionText>
+      </div>
     </div>
 
     <div className="shape_pos_small_triangle">
@@ -60,197 +127,6 @@ const FutureLayers = ({
     </div>
 
     <div className="clear-fix" />
-
-    <style jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-      }
-
-
-      div.clear-fix {
-        clear: both;
-      }
-    
-      /** Shape*/
-      div.shape_pos_small_triangle{
-        position: absolute;
-        top: 19%;
-        left: 90%;
-        z-index: 100;
-
-    }
-
-    @media (max-width: 1080px) {
-      div.shape_pos_small_triangle{
-        top: 19%;
-        left: 80%;
-
-    }
-  }
-    @media (max-width: 600px) {
-      div.shape_pos_small_triangle{
-        top: 10%;
-        left: 70%;
-
-    }
-  }
-    @media (max-width: 600px) {
-      div.shape_pos_small_triangle{
-        top: 6%;
-        left: 70%;
-
-    }
-    }
-
-
-
-    /**  Section Contetn */
-
-      section.future_layer {
-        width: 100%;
-        // height: 970px;
-        position: relative;
-        padding: 110px 0;
-        padding-left: 0;
-        z-index: 1;
-        margin-top: 330px;
-      
-        background: linear-gradient(311.59deg, #28d8a7 0%, #1dc9ca 100%);
-      }
-      
-        // margin-top: 200px;
-        @media (max-width: 1080px) {
-          section.future_layer {
-          padding: 110px 0;
-          margin-top: 250px;
-        }
-      }
-        @media (max-width: 768px) {
-          section.future_layer {
-          padding: 80px 0;
-        }
-      }
-      
-        @media (max-width: 600px) {
-          section.future_layer {
-          margin-top: 200px;
-      
-          padding: 60px 0;
-        }
-      }
-        @media (max-width: 376px) {
-          section.future_layer {   margin-top: 150px;
-      
-          padding: 30px 0;
-        }}
-
-
-
-      div.section_content{
-        height: auto;	
-        width: 55%;	
-        background-color: #FFFFFF;
-        box-shadow: 0px 0px 12px -4px rgba(0,0,0,0.35);
-        margin-right: 8%
-        position: relative;
-        z-index: 1001;
-        float: right;
-        padding: 95px 86px;
-        // transform: translateX(900px);
-        // opacity: 0;
-        transition: all .3s ease-out;
-      }
-
-      @media (max-width: 1080px) {
-        
-      div.section_content{
-        
-        width: 80%;	
-        margin-right: 10%;
-        padding: 80px 0;
-      }
-      div.section_content h2 {
-        margin-top: 0;
-      }
-      
-      }
-
-/************** Sections Image**********************' */
-      
-div.image div.clear_fix{
-  clear: both;
-}
-
-img.content_image {
-  width: 60%;
-  position: absolute;
-  left: 0%;
-  z-index: -1;
-  margin-top: -300px;
-  transition: 0.3s;
-  box-shadow: 5px 5px 10px -4px rgba(0, 0, 0, 0.15);
-}
-@media (max-width: 1440px) {
- img.content_image {
-  width: 70%;
-    
-  }
-}
-
-@media (max-width: 1250px) {
-  img.content_image {
-    width: 70%;
-      
-    }
- }
-
-@media (max-width: 1080px) {
- img.content_image {
-    position: absolute;
-    top: -220px;
-    margin: 0;
-    float: left;
-    left: 50%;
-    transform: translateX(-50%);
-    width: 70%;
-  }
-}
-
-@media (max-width: 750px) {
-   img.content_image {
-    top: -180px;
-  }
-}
-
-@media (max-width: 600px) {
-   img.content_image {
-    position: absolute;
-    top: -140px;
-  }
-}
-@media (max-width: 500px) {
-   img.content_image {
-    position: absolute;
-    top: -100px;
-  }
-}
-
-@media (max-width: 470px) {
-   img.content_image {
-    position: absolute;
-    top: -110px;
-  }
-}
-
-@media (max-width: 320px) {
- img.content_image {
-    position: absolute;
-    top: -70px;
-  }
-}
-    `}</style>
   </section>
 );
 
