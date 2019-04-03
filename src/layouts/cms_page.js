c80ee647f8ccaaff785d4a9c5ef302ca8c007783
editagud <edit0796@stud.kea.dk>
2019-04-03 17:36:02 +0200

    deleting components that are not used

--- src/layouts/cms_page.js
+++ src/layouts/cms_page.js
@@ -2,12 +2,7 @@
 import * as React from "react";
 import { Layout } from "../layouts/";
 import Header from "../components/Header";
-import FutureImageRight from "../components/future_image_right";
 import ContentImage from "../components/content_image";
-import TallCenteredFuture from "../components/tall_centered_future";
-import TallCenteredFutureButtonTop from "../components/tall_centered_future_button_top";
-import FutureImageRightSecond from "../components/future_image_right_second";
-import FutureImageRightNew from "../components/future_image_right_new";
 import FutureLayers from "../components/future_layers";
 import Contact from "../components/Contact";
 import Testimonials from "../components/testimonials";
@@ -20,15 +15,9 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const components: Array<{ Component: React.Component<*>, id: string }> = [
   { Component: Header, id: "header" },
-  { Component: FutureImageRight, id: "future_image_right" },
+
   { Component: ContentImage, id: "content_image" },
-  { Component: TallCenteredFuture, id: "tall_centered_future" },
-  {
-    Component: TallCenteredFutureButtonTop,
-    id: "tall_centered_future_button_top"
-  },
-  { Component: FutureImageRightSecond, id: "future_image_right_second" },
-  { Component: FutureImageRightNew, id: "future_image_right_new" },
+
   { Component: FutureLayers, id: "future_layers" },
   { Component: Testimonials, id: "testimonials" },
   { Component: PriceCalculator, id: "price-calculator" },
